08204d671005e820197c00576ce69598fe2a570a
兰洋 <lanyang@corp.netease.com>
2023-05-23 00:10:05 +0800

    rename

--- src/components/chat/ChatPlaceholder.tsx
+++ src/components/chat/ChatPlaceholder.tsx
@@ -15,15 +15,15 @@ export default function ChatPlaceholder({}: Props) {
   return ( 
     <div className="flex h-full w-full items-center justify-center">
       <div className="max-w-3xl p-4 text-center text-primary">
-        <h1 className="text-4xl font-medium">QQQQ.GAMES</h1>
+        <h1 className="text-4xl font-medium">QQ.services</h1>
         <p className="mt-4 text-lg">
           {" "} 
           <Link
             href="/playground"
             className="font-medium text-primary hover:underline"
           >
-            Playground
-          </Link> for the next generation of AI-powered
+            Experience the next-gen services empowered by AI now
+          </Link>
         </p>
         <ReactDiffViewer
         oldValue={oldCode}
